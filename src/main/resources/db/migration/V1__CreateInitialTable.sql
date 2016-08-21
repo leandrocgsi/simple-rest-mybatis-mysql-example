00d67cdba7239393ae328c1672af5db6b4b91860
leandrocgsi <leandrocgsi@gmail.com>
2016-08-21 15:52:49 -0300

    Adding new table

--- src/main/resources/db/migration/V1__CreateInitialTable.sql
+++ src/main/resources/db/migration/V1__CreateInitialTable.sql
@@ -1,10 +1,22 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=1
+;
 
-DROP TABLE IF EXISTS USERS;
-
-CREATE TABLE users (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  name varchar(100) NOT NULL,
-  email varchar(100) DEFAULT NULL,
-  PRIMARY KEY (id)
-);
-
+CREATE TABLE `person` (
+	`idPerson` BIGINT(20) NOT NULL AUTO_INCREMENT,
+	`address` VARCHAR(255) NOT NULL,
+	`firstName` VARCHAR(255) NOT NULL,
+	`lastName` VARCHAR(255) NOT NULL,
+	PRIMARY KEY (`idPerson`)
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=1
+;
